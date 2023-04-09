cf55a1dca327e8a89fdcaeca580897b050f86e5e
Alex Lindsay <accounts@alex-lindsay.com>
2023-04-09 12:03:58 -0700

    Tighten up header

---
+++ topmenu.inc.php
@@ -0,0 +1,15 @@
+<?php
+	$sections = array(
+		'Home' => 'index.php',
+		'About' => 'about.php',
+		'Artwork' => 'artwork.php',
+		'Contact' => 'contact.php',
+	);
+?>
+
+	<nav class="topnav">
+		<h1>Alex Lindsay</h1>
+    <ul>
+      <ul><a href="/">Home</a></ul>
+    </ul>
+  </nav>
