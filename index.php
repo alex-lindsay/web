7677c9b8e0815ba8dc2c39f12d4fa3304a821a99
Alex Lindsay <accounts@alex-lindsay.com>
2023-04-08 18:14:04 -0700

    Cleanup commented out header code

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 <head>
   <title>Alex Lindsay</title>
   <meta charset="utf-8" />
-  <!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo WEBROOT; ?>/img/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo WEBROOT; ?>/img/ico/apple-touch-icon-144-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo WEBROOT; ?>/img/ico/apple-touch-icon-114-precomposed.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/<?php echo WEBROOT; ?>img/ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="<?php echo WEBROOT; ?>/img/ico/apple-touch-icon-57-precomposed.png"> -->
+	<link rel="apple-touch-icon-precomposed" href="<?php echo WEBROOT; ?>/img/ico/apple-touch-icon-57-precomposed.png">
 	<link rel="shortcut icon" href="<?php echo WEBROOT; ?>/img/favicon.ico">
   <link rel="stylesheet" href="<?php echo WEBROOT; ?>/css/main.css" media="screen">
   <link rel="preconnect" href="https://fonts.googleapis.com"> 
