fa620a59cc84e1b77e8782097cf90d15ba219877
Alex Lindsay <accounts@alex-lindsay.com>
2023-05-16 09:53:45 -0700

    image thumbnails

--- interests.php
+++ interests.php
@@ -12,7 +12,7 @@ $pageclass = 'interests';
   <main>
     <h1>Alex Lindsay — Interests</h1>
     <article>
-      <a href="assets/img/art/MilanPortaGaribaldi-signed.jpeg" title="Link to larger copy of 'Self Portrait at the Porta Garabaldi aimed at the sky' by Alex Lindsay"><img style="float: left; height: 16rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/MilanPortaGaribaldi-signed.jpeg" title="Self Portrait at the Porta Garabaldi aimed at the sky by Alex Lindsay" /></a>
+      <a href="assets/img/art/MilanPortaGaribaldi-signed.jpeg" title="Link to larger copy of 'Self Portrait at the Porta Garabaldi aimed at the sky' by Alex Lindsay"><img style="float: left; height: 16rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/MilanPortaGaribaldi-signed-thumb.jpeg" title="Self Portrait at the Porta Garabaldi aimed at the sky by Alex Lindsay" /></a>
       <p>One of the things I hope to accomplish with this website, is to give anyone who might wander in here a glimpse of some of the things that have caught my fancy over the years.</p>
       <p>I describe myself as a generalist, and here you'll find the tip of the iceberg of some of the islands in the general floe. There are any number of additional interests and curiosities that I have that I may add over time. Or may not.</p>
       <p>I hope to add further info in more detailed pages for some of the topics that really grab my engagement, connecting to other people, other sites which have inspired and delighted me throughout my life.</p>
@@ -29,7 +29,7 @@ $pageclass = 'interests';
     </article>
     <h2 id="digital-art">Digital Art</h2>
     <article>
-      <a href="assets/img/art/flow 1.jpg" title="Link to larger copy of 'Flow 1' by Alex Lindsay"><img style="float: left; height: 16rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/flow 1.jpg" title="Flow 1 by Alex Lindsay" /></a>
+      <a href="assets/img/art/flow 1.jpg" title="Link to larger copy of 'Flow 1' by Alex Lindsay"><img style="float: left; height: 16rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/flow 1-thumb.jpeg" title="Flow 1 by Alex Lindsay" /></a>
       <p>A number of years ago, when I first transitioned from being a hands-on manager to being a hands-off one, focusing on team development and project success, I found myself itching to do some coding.</p>
       <p>I would work on coding puzzles, and some personal projects, but I was feeling a different sort of challenge facing me as well.</p>
       <p>As I mention elsewhere, I come from parents who were writers. But, my father was also a painter. My aunt, a painter and sculptor as well. My grandfather, a talented calligrapher. Throughout my family there were (and are) a variety of talented artists. I felt challenged to create.</p>
@@ -37,7 +37,7 @@ $pageclass = 'interests';
     </article>
     <h2 id="photography">Photography</h2>
     <article>
-      <a href="assets/img/art/BarcelonaPavilionWithDave-signed.jpeg" title="Link to larger copy of 'Barcelona Pavilion with Dave' by Alex Lindsay"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/BarcelonaPavilionWithDave-signed.jpeg" title="Barcelona Pavilion with Dave by Alex Lindsay" /></a>
+      <a href="assets/img/art/BarcelonaPavilionWithDave-signed.jpeg" title="Link to larger copy of 'Barcelona Pavilion with Dave' by Alex Lindsay"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/BarcelonaPavilionWithDave-signed-thumb.jpeg" title="Barcelona Pavilion with Dave by Alex Lindsay" /></a>
       <p>I have had an interest and engagement with photography throughout my life. Starting with my first film SLR camera, which I still have, through the arrival of digital SLRs, pocket digital cameras, through to the marvel that is the collection of cameras we have available to us on our contemporary phones.</p>
       <p>I think there were two factors that came into play for me here: differentiating my creativity from others in my family, and an attraction to the intersection of art and technology.</p>
       <p>Early on, I took photos of cars and landmarks, and some people here and there, but I found myself drawn to shooting architecture.</p>
@@ -46,7 +46,7 @@ $pageclass = 'interests';
     </article>
     <h2 id="architecture">Architecture</h2>
     <article>
-      <a href="assets/img/art/VictoriaAndAlbertMuseum-signed.jpeg" title="Link to larger copy of 'Victoria and Albert Museum' by Alex Lindsay"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/VictoriaAndAlbertMuseum-signed.jpeg" title="Victoria and Albert Museum by Alex Lindsay" /></a>
+      <a href="assets/img/art/VictoriaAndAlbertMuseum-signed.jpeg" title="Link to larger copy of 'Victoria and Albert Museum' by Alex Lindsay"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/VictoriaAndAlbertMuseum-signed-thumb.jpeg" title="Victoria and Albert Museum by Alex Lindsay" /></a>
       <p>I can't put a finger quite to how I became interested in architecture, but I was certainly influenced by the work that my grandfather did in running a construction company that built custom homes.</p>
       <p>For me the tehnical drawings and blueprints were fascinating. Precise, geometric, explicit. Those designs were a way to communicate what would be translated into sculpture. Functional scultpure — which could be lived in, worked in.</p>
       <p>The part of me that was drawn to technology and futurism found an attraction for the stules that were presented as futuristic and forward looking.</p>
@@ -54,7 +54,7 @@ $pageclass = 'interests';
     </article>
     <h2 id="typography">Typography</h2>
     <article>
-      <a href="assets/img/Letraset_Sample.jpg" title="Link to larger copy of 'Sample of Helvetica Bold Condensed type by Letraset'"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/Letraset_Sample.jpg" title="Sample of Helvetica Bold Condensed type by Letraset" /></a>
+      <a href="assets/img/Letraset_Sample.jpg" title="Link to larger copy of 'Sample of Helvetica Bold Condensed type by Letraset'"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/Letraset_Sample-thumb.jpeg" title="Sample of Helvetica Bold Condensed type by Letraset" /></a>
       <p>My parents ran an advertising and marketing agency when I was young. In an era before computers and word processing, copy layout would be done by hand, literally cutting and pasting text and images or sketches to build out concepts or templates.</p>
       <p>To facilitate with mocking up copy or designs, there was a product called <a href="https://en.wikipedia.org/wiki/Letraset">Letraset</a> which would allow someone to rub letters of different fonts in different sizes onto paper.</p>
       <p>I was fascinated by these, and by the type books that we had in the house, used for selecting fonts to be used for different ad concepts.</p>
@@ -64,7 +64,7 @@ $pageclass = 'interests';
     </article>
     <h2 id="geography">Geography</h2>
     <article>
-      <a href="assets/img/art/MiamiFromTheAir-signed.jpeg" title="Link to larger copy of 'Miami from the Air'"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/MiamiFromTheAir-signed.jpeg" title="Miami from the Air by Alex Lindsay" /></a>
+      <a href="assets/img/art/MiamiFromTheAir-signed.jpeg" title="Link to larger copy of 'Miami from the Air'"><img style="float: left; height: 18rem; margin: 1rem 1rem 1rem 0" src="assets/img/art/MiamiFromTheAir-signed-thumb.jpeg" title="Miami from the Air by Alex Lindsay" /></a>
       <p>Geography is an interesting beast. For many, it calls to mind maps, maps, and still more maps. For others it encompasses the physical world we live in — the rivers, the mountains, the islands, the oceans. For still others, it's about the people, the places, the communities, the cultures.</p>
       <p>I started out a map geek, going back to learning the counties of the State of New Jersey, the capitals of the states. I always wanted an atlas; collected a few road atlases, world atlases, printed, eventually digital...until the internet made that unnecessary.</p>
       <p>As I've grown older, I've found so much more interest in the political geography, the cultural geography — the ways in which we've mapped our selves onto the terrain we inhabit.</p>
